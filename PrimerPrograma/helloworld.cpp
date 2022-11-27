1127302b2210c4b96ac8e2a44139d0bc948fc57e
arion5559 <42389714+arion5559@users.noreply.github.com>
2022-11-27 17:45:11 +0100

    Variables y primeros pasos

---
+++ PrimerPrograma/helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    float a = 0.12345678901234567890f;
+    double b = 0.12345678901234567890;
+    char c = 'a';
+    char curr = '$';
+    bool student = false;
+    std::string name = "John";
+    std::string food = "Pasta";
+
+    std::cout << student << std::endl;
+    std::cout << b << std::endl;
+    std::cout << a << std::endl;
+    std::cout << c << std::endl;
+    std::cout << curr << std::endl;
+    std::cout << name << std::endl;
+    return 0;
+}
