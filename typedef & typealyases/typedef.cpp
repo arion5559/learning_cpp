61b5be4ff13ec2bce4baa5d4323d7ee2da8118c3
arion5559 <42389714+arion5559@users.noreply.github.com>
2022-11-27 19:31:37 +0100

    namespaces y typedef

---
+++ typedef & typealyases/typedef.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+
+// typedef std::string text_t;
+
+using text_t = std::string;
+
+int main() {
+    text_t text = "Hello, world!";
+
+    std::cout << text << std::endl;
+
+    return 0;
+}
